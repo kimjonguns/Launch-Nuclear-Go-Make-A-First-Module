1b1b7704a20bb719207f657aa6ca0b015b8a2c0f
kimjonguns <hafiihza6@gmail.com>
2022-12-04 11:00:29 +0700

    pertama

---
+++ app.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+
+	sayhai "github.com/kimjonguns/Nuclear-Go-Make-A-First-Module/v2"
+)
+
+func main() {
+	fmt.Println(sayhai.Sayhai("Hafi"))
+	fmt.Println(sayhai.NuclearLaunch())
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/kimjonguns/Launch-Nuclear-Go-Make-A-First-Module
+
+go 1.19
+
+require github.com/kimjonguns/Nuclear-Go-Make-A-First-Module/v2 v2.0.1 // indirect
